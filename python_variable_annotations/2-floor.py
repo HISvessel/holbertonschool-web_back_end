1e1d7d5e6e08c0a0f346f0fd32527ff1010a9f36
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 00:08:44 -0400

    this script takes a float and returns the whole value of a given number

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this script takes a float and returns the floor of the value"""
+
+
+def floor(n: float) -> int:
+    """this function returns the floor of an integer"""
+    return int(n)
