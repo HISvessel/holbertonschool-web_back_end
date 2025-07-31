f77e639ec1b875aa56cadf6311fd9ed62a7db5a6
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 00:18:26 -0400

    correct expected type annotation for every variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """this script studies the concept of typing"""
 
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = bool(True)
-school = str("Holberton")
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
