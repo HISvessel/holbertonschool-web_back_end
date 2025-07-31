27317e5ac5a33edc852db4a2847f92083d0cda46
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 01:35:18 -0400

    codestyle fix

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -12,5 +12,5 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
 
     def multiplier_func(x: float) -> float:
         return x * multiplier
-    
+
     return multiplier_func
