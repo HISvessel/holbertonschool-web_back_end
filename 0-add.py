a86963adbf3d67b10a7e5f78cf51ba5c411fea90
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-30 23:39:11 -0400

    this script creates a sun between two floats and only between two floats

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this function introduces the concept of type annotation
+
+this is a more concrete form of documentation and error handling
+of dynamic types. We are essentialy indicating with this that we are expecting
+in a very concrete and concise manner that we are expecting a sum and a 
+result of floats, and if any other data type is passed, an error is thrown"""
+
+def add(a: float, b: float) -> float:
+    return a + b
