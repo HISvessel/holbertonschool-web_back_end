6d5b918fe378a58f6da4bd6cf4f792f75058f725
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 00:34:13 -0400

    pycodestyle fix

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,7 @@ list and returns the sums of its indeces"""
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """this function returns the sum of a list
     of floats, reinforced with type annotation."""
