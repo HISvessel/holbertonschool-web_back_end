1207f64a37a26ebe55ec3e9914ef9decd11f30fa
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 00:59:26 -0400

    eturning a single type from a combinatino ]]]]]n of multiple numbers and mltiple types

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""this function takes a mixed list of numbers
+and returns their sum as a float"""
+
+from typing import List, Union
+
+
+def sum_mixed_lsit(mxd_list: List[Union[int, float]]) -> float:
+    """this function returns a float of a combination of
+    mixed integers and floats in a single list"""
+    return float(sum(mxd_list))
