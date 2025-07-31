f5daf2f910ab8d9398a502d6283ae9388c5fd8ad
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 00:11:49 -0400

    this script takes a float unmber and returns it as a string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this function takes an integer and converts it to a string
+reinforced with type annotation"""
+
+
+def to_str(n: float) -> str:
+    """takes an float and returns a string"""
+    return str(n)
