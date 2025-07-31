95051a5a96b66a8db880577d68c4594a9153cb6e
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 00:03:43 -0400

    this script concatenates two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""this function enforces type annotation on function parameters"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns the concatenation of two strings
+    the function is reinforced with type annotation"""
+
+    return str1 + str2
