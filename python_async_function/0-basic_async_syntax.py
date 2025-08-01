eb210fd7e4bfb3343954abf50dd480141a3bbda3
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-31 23:35:12 -0400

    this script returns the value of the random generated delay time.

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+"""this script contains a function that generates a random
+delay time"""
 import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """the function waits a random amount of time before returning
+    the delayed value for which the time was spent"""
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
