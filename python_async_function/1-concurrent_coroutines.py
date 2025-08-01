72d278045d1344c161ea235e4b0383223be3dd57
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-01 00:05:14 -0400

    sequence of timed concurrencies

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+"""this script contains a function that creates a list of
+concurrencies and annotates them in sequential order"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """this function returns a list of concurrencies. The
+    times are appended in the orther in which the occur and the ammount
+    is dependant on the range given by the n parameter"""
+    
+    timer = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    time_list: List = []
+    for completed_future in asyncio.as_completed(timer):
+        delay = await completed_future
+        time_list.append(delay)
+    return(time_list)
