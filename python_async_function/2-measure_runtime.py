488b8ec587e1515443725c8ef9c77e18e16d4b6d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-01 00:33:40 -0400

    creating a non async function that takes the timing of an asynchronous function

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""this script contains the function called measure_time that takes
+an integer and returns the total elapsed time of a given set of concurrencies"""
+
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """this function returns the total elapsed timed for the wait_n
+    function to complete given an n index and a delay time"""
+    clock_in = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    clock_out = time.time() - clock_in
+    return clock_out / n
+
+
+n = 5
+max_delay = 9
+
+print(measure_time(n, max_delay))
+
