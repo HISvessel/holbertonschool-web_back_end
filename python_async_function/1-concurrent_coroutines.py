28329748e56fd2631f48c22c8460298eba142b47
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-01 00:06:59 -0400

    pycodestyle fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@ from typing import List
 """this script contains a function that creates a list of
 concurrencies and annotates them in sequential order"""
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -11,7 +12,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """this function returns a list of concurrencies. The
     times are appended in the orther in which the occur and the ammount
     is dependant on the range given by the n parameter"""
-    
+
     timer = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     time_list: List = []
     for completed_future in asyncio.as_completed(timer):
