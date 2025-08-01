05d9dfec92495c0f2268a671003aa3466608d0e0
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-01 01:45:24 -0400

    fixes

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,11 +10,12 @@ from typing import List
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) ->List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """this task performs a series of sequences and stores them
     in a list. these sequences are retrieved from another module
     and replicate imported behaviour, showing encapsulation"""
-    task: List[asyncio.Task[float]] = [task_wait_random(max_delay) for _ in range(n)]
+    task: List[asyncio.Task[float]] = [task_wait_random
+                                       (max_delay) for _ in range(n)]
     task_load: List[float] = []
     for completed_task in asyncio.as_completed(task):
         delay = await completed_task
