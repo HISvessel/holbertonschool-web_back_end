ef171aa3fbdea454a59710fec0aa4884297f1a94
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-01 22:55:12 -0400

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,4 +10,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """this function generates a routine"""
+
     return [value async for value in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""this task measure the suntime between four parallel asyncs"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Run async_comprehension() four times in parallel, measure elapsed time,
+    and return it.
+
+    Returns:
+        float: Total time in seconds to complete all four runs.
+    """
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.time()
+    return end - start
