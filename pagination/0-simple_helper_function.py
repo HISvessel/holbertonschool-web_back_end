0a0a822db9cf489e376be2c1500080e489672b33
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-11 16:33:58 -0400

    added csv file for project. Fixed function to test checkers and logic

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,13 +5,7 @@ two arguments: page and page_size"""
 
 
 from typing import Tuple
+
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     return (page, page_size * page)
-
-res = index_range(1, 7)
-print(type(res))
-print(res)
-
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
