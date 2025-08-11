abe76a7d66a366b6402580b236c72dea003149c1
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-11 17:25:04 -0400

    testing for checkers

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,4 +8,8 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    return ((page - 1) * page_size, page_size * page)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+    
+    # return ((page - 1) * page_size, page_size * page)
