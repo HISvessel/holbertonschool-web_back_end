e63e9034295d893b7b8cb0f240843e8df5c07971
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-11 17:27:18 -0400

    added documentation, checking if checkers oblige

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,6 +8,9 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """this function returns the pagination layout for a
+    page given its amount of pages and size"""
+
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
