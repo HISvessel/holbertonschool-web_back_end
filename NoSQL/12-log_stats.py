f7e6990701c8e442f9e68960a777c2b39c16f1d8
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-17 22:08:04 -0400

    this script prints the logs stored as part of the nginx reverse roxy searcher for mongodb. We can see how many methods and which methods are being utilized

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""this script provides information about Nginx as stored
+in MongoDB"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+
+    db = client.logs
+    collection = db.nignx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+
+    print("Methods:/")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({"methods": method})
+        print(f"\t method {method}: {count}")
+    
+    status_check = collection.count_document(
+        {"method": "GET", "path": "\status"})
+    print(f"{status_check} status check")
