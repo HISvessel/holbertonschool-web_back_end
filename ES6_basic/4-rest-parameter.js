992ad3e74f6cbb5acecf57c87565b9be7a8a1ea1
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-19 10:37:30 -0400

    es linter fix. testing checkers

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...manyArgs) {
   let counter = 0;
-  for (const arg of manyArgs){
+  for (const arg of manyArgs) {
     counter += 1;
   }
   return counter;
