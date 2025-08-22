b53fe26baab1b017ddecc37ff735679473f63505
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-21 23:35:03 -0400

    nested class method fixed

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from "./3-currency.js"
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = new Currency(currency.code, currency.name);
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(input) {
+    this._amount = input;
+  }
+
+    get currency() {
+    return this._currency;
+  }
+
+  set currency(input) {
+    this._currency = input;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+  static convertPrize(amount, conversionRate) {
+    if (typeof amount !== "number" || typeof conversionRate !== "number") {
+      console.error("Parameters must be numbers")
+    }
+    return amount * conversionRate;
+  }
+}
