84fa20349143fdf87ee17433b078dcfb4bce2de7
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-24 17:46:31 -0400

    further testing

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,6 +1,6 @@
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = Number(size);
-    this._location = String(location);
+    this._location = toString(location);
   }
 }
