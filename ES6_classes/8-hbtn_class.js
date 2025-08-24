370970e71fc3663cd9c19bf91ef0074880c9fe87
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-24 17:54:01 -0400

    the key is to implement global functions on to class templates. further testing answers and checkers

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,9 +1,11 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this._size = Number(size);
+    this._size = size;
     this._location = location;
   }
-
+  Number() {
+    return this._size;
+  }
   toString() {
     return this._location;
   }
