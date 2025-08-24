58435163e10e2632e5dd8910bd3fdca944ed8364
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-24 17:47:47 -0400

    added new way to test type conversion

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,6 +1,10 @@
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = Number(size);
-    this._location = toString(location);
+    this._location = location;
+  }
+
+  toString() {
+    return this._location;
   }
 }
