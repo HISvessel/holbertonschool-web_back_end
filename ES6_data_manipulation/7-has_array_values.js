2fd5e428a544ba968ff553792cf3fd4de7698377
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-27 23:22:01 -0400

    testing checkers

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  const newSet = new Set(array);
-  const diff = newSet.difference(set);
-  if (diff.size !== 0) {
-    return false;
-  }
-  return true
+ return set.has(...array)
 }
