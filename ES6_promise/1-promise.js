5f9b0a89b34154ab4554f8d1790e4f6db0782b82
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-29 13:37:25 -0400

    this script contains a function that returns a promise based on the parameter's boolean value. If the success sis true, it returns the promise, otherwise it returns the rejected promise with an error message

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((response, reject) => {
+    if (success == false) {
+      reject({Error :"The fake API is not working currently"});
+    }
+    response({
+      status: 200,
+      body: "Success"
+    });
+  });
+  return promise;
+}
