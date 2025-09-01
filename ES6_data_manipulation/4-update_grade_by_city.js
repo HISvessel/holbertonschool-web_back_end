5d95257fca86891f011368d09a192fd9431b412f
HISvessel <kevinsanchezdabest@yahoo.com>
2025-08-31 21:35:48 -0400

    this script contains a function that returns a new object where the original list of students is given a new object entry of the newGrades object. If a grade is not given as an argument, the function returns n/A instead.

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,14 @@
 import getListStudents from "./0-get_list_students";
 
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-    if (newGrades === undefined) {
-        return `N/A`;
-    }
-    const filteredStudents = getListStudents.filter((student) => student["location"] === city)
-    const grade = newGrades.map((g) => g["grade"]);
-    return filteredStudents.map((student) => student["grade"] = grade);
+  const filteredStudents = getListStudents.filter((student) => student["location"] === city)
+  .map(student => {
+    const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+    return {
+      ...student, 
+      grade: gradeObj ? gradeObj.grade : "N/A",
+      };
+    });
+
+    return filteredStudents;
 }
