ebeff83040f3f812b3f370e097585710fbd7ea7b
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-01 22:25:25 -0400

    the process loops indefinitely with various names being accepted. program does not close dynamically, only through pipleines

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,10 +4,14 @@ process.stdout.write("Welcome to Holberton School, what is your name? \n");
 process.stdin.on("data", (input) => {
   const name = input.toString().trim();
   process.stdout.write(`Your name is: ${name} \n`);
-  const child = spawn("echo", ["This important software is now closing"]);
-  child.stdout.pipe(process.stdout);
-  child.on("close", () => {
-    process.exit(0);
-  });
+  //const child = spawn("echo", ["This important software is now closing"]);
+  //child.stdout.pipe(process.stdout);
+  //child.on("close", () => {
+//    process.exit(0);
+//  });
+});
+
+process.on("exit", () => {
+  console.log("This important software is now closing");
 });
 
