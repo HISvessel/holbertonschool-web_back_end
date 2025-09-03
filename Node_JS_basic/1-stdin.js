d38b55117c58e6b104c6fffeef2177fc9f24d563
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-02 22:25:56 -0400

    possible linter fix, testing checkers

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,5 @@
 console.log("Welcome to Holberton School, what is your name?");
-
 process.stdin.setEncoding('utf-8');
-
 process.stdin.on("data", (input) => {
   process.stdout.write(`Your name is: ${input}`);
 });
