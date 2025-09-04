b34673b63946b4c650a68e618c064985bd087612
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-04 14:58:27 -0400

    linter fix

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,19 +2,19 @@ function countStudents(path) {
   const fs = require('node:fs');
   try {
     const file = fs.readFileSync(path, 'utf-8');
-    const lines = file.split("\n").map((l) => l.trim()).filter((l) => l.length > 0);
+    const lines = file.split('\n').map((l) => l.trim()).filter((l) => l.length > 0);
     const rows = lines.slice(1);
-    const fieldRows = rows.map(student => { 
+    const fieldRows = rows.map(student => {
       const name = student.split(',')[0];
       const field = student.split(',')[3];
-      return [name, field]});
+      return [name, field];
+    });
     const CSStudents = fieldRows.filter(student => student[1] === 'CS');
-    const SWEStudents = fieldRows.filter(student => student[1] === 'SWE')
+    const SWEStudents = fieldRows.filter(student => student[1] === 'SWE');
 
-  console.log(fieldRows);
-  console.log(`Number of students: ${rows.length}`);
-  console.log(`Number of Students in CS: ${CSStudents.length}. List: ${CSStudents.map(student => student[0]).join(', ')}`);
-  console.log(`Number of students in SWE: ${SWEStudents.length}. List: ${SWEStudents.map(student => student[0]).join(', ')}`);
+    console.log(`Number of students: ${rows.length}`);
+    console.log(`Number of students in CS: ${CSStudents.length}. List: ${CSStudents.map(student => student[0]).join(', ')}`);
+    console.log(`Number of students in SWE: ${SWEStudents.length}. List: ${SWEStudents.map(student => student[0]).join(', ')}`);
   } catch {
     throw new Error('Cannot load database');
   }
