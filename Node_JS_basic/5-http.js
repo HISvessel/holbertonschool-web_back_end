e8d7b02bf8abb2059ef80ff337bbc4aa6d72986f
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-05 15:51:39 -0400

    added further logic

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,17 +1,26 @@
 const http = require('node:http');
 const countStudents = require('./3-read_file_async');
-const path = 'database.csv'
+const path = 'database.csv';
+const util = require('util');
 
 const app = http.createServer(async (req, res) => {
-  //const content = await countStudents(path);
-
-  if (req.url == '/') {
+  if (req.url === '/') {
     res.writeHead(200, { 'content-type' : 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (req.url == '/students') {
-    const content = await countStudents(path).toString();
+  } else if (req.url === '/students') {
+    const originalLog = console.log;
+    let logBuffer = '';
+
+    console.log = (...args) => {
+      const message = util.format(...args);
+      logBuffer += message + '\n';
+      originalLog.apply(console, args);
+    };
+
+    const content = await countStudents(path);
+    console.log = originalLog;
     res.writeHead(200, { 'content-type' : 'text/plain' });
-    res.end(content);
+    res.end(`This is the list of our students\n${logBuffer}`);
   } else {
     res.writeHead(404, { 'content-type' : 'text/plain' });
     res.end('404 Not found');
