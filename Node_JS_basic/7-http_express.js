621964c3651d97b9c79085c33926783583005db3
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-05 23:42:25 -0400

    this node script contains a functionthat returns an app with two http ports that sedn responses of the database. debugging database access and functionality

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = 'database.csv';
+const port = 1245;
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const content = await countStudents(path);
+  res.end(`This is the list of our students\n ${content}`);
+});
+
+app.listen(port);
+
+module.exports = app;
