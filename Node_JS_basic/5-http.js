734d84541b4a00624efcfb06e4d5e173141930bc
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-06 01:35:57 -0400

    linter fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('node:http');
 const countStudents = require('./3-read_file_async');
-const path = process.argv[2];
 
+const path = process.argv[2];
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'content-type': 'text/plain' });
@@ -12,8 +12,8 @@ const app = http.createServer(async (req, res) => {
       res.writeHead(200, { 'content-type': 'text/plain' });
       res.end(`This is the list of our students\n${content}`);
     } catch (e) {
-      res.writeHead(400, { 'content-type': 'text/plain' });
-      res.end(`This is the list of our students\n${e.toString().replace('Error: ', '' )}`);
+      res.writeHead(500, { 'content-type': 'text/plain' });
+      res.end(`This is the list of our students\n${e.toString().replace('Error: ', '')}`);
     }
   } else {
     res.writeHead(404, { 'content-type': 'text/plain' });
